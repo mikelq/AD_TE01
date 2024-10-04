81e8b1ed271d138afa5ebeceb572b98ed6b165f4
Mikel <Mikel@DESKTOP-VF0LKT2>
2024-10-04 19:04:50 +0200

    commit 2 ejer

--- src/ad_te01/Ejer2.java
+++ src/ad_te01/Ejer2.java
@@ -5,10 +5,26 @@ import java.io.*;
 
 public class Ejer2 {
 	public static void main (String[] args) throws IOException {
+		try {	
+			BufferedReader ficheroE = new BufferedReader(new FileReader("C:\\Users\\Mikel\\Documents\\DAM\\AD\\src\\ad_te01\\fichero_entrada.txt"));
+			BufferedWriter ficheroS= new BufferedWriter (new FileWriter("C:\\Users\\Mikel\\Documents\\DAM\\AD\\src\\ad_te01\\fichero_salida.txt"));
 			
-			File ficheroE = new File ("C:\\Users\\Mikel\\Documents\\DAM\\AD\\src\\ad_te01\\fichero_entrada.txt");
-			File ficheroS = new File ("C:\\Users\\Mikel\\Documents\\DAM\\AD\\src\\ad_te01\\fichero_salida.txt");
-			FileReader ficE = new FileReader(ficheroE);
-			FileWriter ficS = new FileWriter(ficheroS);
+			String ln;
+			while((ln = ficheroE.readLine()) != null) {
+				
+				String[] lnEntera = ln.split(" ");
+				
+				String nombre = lnEntera[0];
+				if(nombre.length() == 5) {
+					ficheroS.write(nombre);
+					ficheroS.newLine();
+				}
+			}
+			ficheroE.close();
+			ficheroS.close();
+		}catch(FileNotFoundException fn) {
+			System.out.println("No se ha encontrado archivo");
+		}
+			
 	}
 }
